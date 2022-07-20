4c93389d6d80ea8b003fe48bdcda7eb5aa167952
Aparna Priyadarshani <apriyadarshani925@gmail.com>
2022-07-20 22:38:51 +0530

    helloworld

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { FormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
@@ -24,6 +25,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     MatIconModule,
     MatDividerModule,
     FormsModule,
+    MatListModule,
     BrowserAnimationsModule
   ],
   providers: [],
